59282e0117d037e8bf2f6b25c96b1d79fcc0ead5
apraxo <114722326+apraxo@users.noreply.github.com>
2024-07-25 21:08:40 -0600

    Add files via upload

---
+++ build/.qt/QtDeployTargets-MinSizeRel.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_AimmyV2_CPP_FILE C:/Amm/etc/Aimmy2_CPP/build/MinSizeRel/AimmyV2_CPP.exe)
+set(__QT_DEPLOY_TARGET_AimmyV2_CPP_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_AimmyV2_CPP_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)

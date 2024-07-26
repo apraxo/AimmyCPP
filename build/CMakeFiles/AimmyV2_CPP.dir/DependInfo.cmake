6ce30d27f8dfa35c265a4f19d32733505ca1d1c2
apraxo <114722326+apraxo@users.noreply.github.com>
2024-07-25 21:29:26 -0600

    Add files via upload

---
+++ build/CMakeFiles/AimmyV2_CPP.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "AimmyV2_CPP_autogen/timestamp" "custom" "AimmyV2_CPP_autogen/deps"
+  "C:/Amm/etc/Aimmy2_CPP/build/AimmyV2_CPP_autogen/mocs_compilation.cpp" "CMakeFiles/AimmyV2_CPP.dir/AimmyV2_CPP_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/AimmyV2_CPP_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/predictions/LeadTargetPrediction.cpp" "CMakeFiles/AimmyV2_CPP.dir/predictions/LeadTargetPrediction.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/predictions/LeadTargetPrediction.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/AIModel.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/AIModel.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/AIModel.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/Configs.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/Configs.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/Configs.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/Fonts.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/Fonts.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/Fonts.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/InputBindingManager.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/InputBindingManager.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/InputBindingManager.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/KeybindEventFilter.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/KeybindEventFilter.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/KeybindEventFilter.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/MainWindow.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/MainWindow.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/MainWindow.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/OverlayWindow.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/OverlayWindow.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/OverlayWindow.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/PredictAheadTarget.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/PredictAheadTarget.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/PredictAheadTarget.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/PredictionManager.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/PredictionManager.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/PredictionManager.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/RequirementsManager.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/RequirementsManager.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/RequirementsManager.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/SecondaryWindow.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/SecondaryWindow.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/SecondaryWindow.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/Settings.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/Settings.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/Settings.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/UserController.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/UserController.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/UserController.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/Visualization.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/Visualization.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/Visualization.cpp.obj.d"
+  "C:/Amm/etc/Aimmy2_CPP/src/main.cpp" "CMakeFiles/AimmyV2_CPP.dir/src/main.cpp.obj" "gcc" "CMakeFiles/AimmyV2_CPP.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

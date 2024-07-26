8ecf23663824b75f107198b910cd8ea1d479b821
apraxo <114722326+apraxo@users.noreply.github.com>
2024-07-25 21:33:57 -0600

    Add files via upload

---
+++ build/CMakeFiles/AimmyV2_CPP_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AimmyV2_CPP_autogen/include/src/ui_MainWindow.h"
+  "AimmyV2_CPP_autogen/include/src/ui_OverlayWindow.h"
+  "AimmyV2_CPP_autogen/include/src/ui_SecondaryWindow.h"
+  "AimmyV2_CPP_autogen/mocs_compilation.cpp"
+  "AimmyV2_CPP_autogen/timestamp"
+  "CMakeFiles/AimmyV2_CPP_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/AimmyV2_CPP_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
